8cc63ceaca9312de28dc14c50c41b2193dcffb29
stanlettt <stanletmintsa17@gmail.com>
2026-02-10 15:27:20 +0100

    Refactor clickfunc and increaseSize functions
    Fixed typos in variable names and improved formatting.

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function clickfunc(){
-    window.location.href="desire-oui.html"
+function clickfunc() {
+    window.location.href = "desire-oui.html";
 }
 
-function increaseSize(){
+function increaseSize() {
     const ouiButton = document.querySelector(".oui");
 
-    ouiButton.style.width = (ouiButton.offsetwidth + 70) + 'px';
-    ouiButton.style.height = (ouiButton.offsetHeight + 70) + 'px';
+    ouiButton.style.width = (ouiButton.offsetWidth + 70) + "px";
+    ouiButton.style.height = (ouiButton.offsetHeight + 70) + "px";
 
-    const currentFontSize = window.getComputedStyle(ouiBoutton).fontSize;
-    const newFontSize = parseInt(currentFontSize) + 20 + 'px';
-    ouiButton.style.fontSize = newFontSize
+    const currentFontSize = window.getComputedStyle(ouiButton).fontSize;
+    const newFontSize = parseInt(currentFontSize) + 20 + "px";
+    ouiButton.style.fontSize = newFontSize;
 }
+
+
 
 
 
